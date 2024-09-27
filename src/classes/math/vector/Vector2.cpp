b3a10a43b5e001e127d1d4276ad8c1ee615e11ed
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-09-27 13:17:00 -0400

    math implementations

--- src/classes/math/vector/Vector2.cpp
+++ src/classes/math/vector/Vector2.cpp
@@ -10,4 +10,37 @@ namespace gengine {
 		this->x = x;
 		this->y = y;
     }
+
+	Vector2 Vector2::operator+(const Vector2& other) const
+	{
+		return Vector2(x + other.x, y + other.y);
+	}
+	Vector2 Vector2::operator+(double value) const
+	{
+		return Vector2(x + value, x + value);
+	}
+	Vector2 Vector2::operator-(const Vector2& other) const
+	{
+		return Vector2(x - other.x, y - other.y);
+	}
+	Vector2 Vector2::operator-(double value) const
+	{
+		return Vector2(x - value, y - value);
+	}
+	Vector2 Vector2::operator*(const Vector2& other) const
+	{
+		return Vector2(x * other.x, y * other.y);
+	}
+	Vector2 Vector2::operator*(double value) const
+	{
+		return Vector2(x * value, y * value);
+	}
+	Vector2 Vector2::operator/(const Vector2& other) const
+	{
+		return Vector2(x / other.x, y / other.y);
+	}
+	Vector2 Vector2::operator/(double value) const
+	{
+		return Vector2(x / value, y / value);
+	}
 }
--- src/classes/math/vector/Vector2.hpp
+++ src/classes/math/vector/Vector2.hpp
@@ -9,7 +9,22 @@ namespace gengine {
             double x, y;
             Vector2();
             Vector2(double x, double y);
-            
+
+            // Addition
+            Vector2 operator+(const Vector2& other) const;
+            Vector2 operator+(double value) const;
+
+            // Subtraction
+            Vector2 operator-(const Vector2& other) const;
+            Vector2 operator-(double value) const;
+
+            // Multiplication
+            Vector2 operator*(const Vector2& other) const;
+            Vector2 operator*(double value) const;
+
+            // Division
+            Vector2 operator/(const Vector2& other) const;
+            Vector2 operator/(double value) const;
     };
 }
 
