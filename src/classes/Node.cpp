42a80f3d67392f937f087c7facef11609d7d0e0f
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-09-27 11:27:27 -0400

    ✨ implementation ✨

--- src/classes/Node.cpp
+++ src/classes/Node.cpp
@@ -1,8 +1,54 @@
 #include "Node.hpp"
 
 namespace gengine {
-    Node::Node() {
+	Node::Node()
+	{
 		this->x = 0;
 		this->y = 0;
-    }
+		this->w = 0;
+		this->h = 0;
+	}
+
+	Node::Node(double x, double y)
+	{
+		this->x = x;
+		this->y = y;
+		this->w = 0;
+		this->h = 0;
+	}
+
+	Node::Node(double x, double y, double w, double h)
+	{
+		this->x = x;
+		this->y = y;
+		this->w = w;
+		this->h = h;
+	}
+
+	Node::Node(Vector2 pos)
+	{
+		this->x = pos.x;
+		this->y = pos.y;
+		this->w = 0;
+		this->h = 0;
+	}
+
+	Node::Node(Vector2 pos, Vector2 size)
+	{
+		this->x = pos.x;
+		this->y = pos.y;
+		this->w = size.x;
+		this->h = size.y;
+	}
+
+
+	Vector2 Node::getPosition()
+	{
+		return Vector2(this->x, this->y);
+	}
+
+	Vector2 Node::getScale()
+	{
+		return Vector2(this->w, this->h);
+	}
 }
