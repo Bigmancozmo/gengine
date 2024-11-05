12116814a2cbf998be555287cf6a1cf849f09912
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-11-05 18:29:07 -0500

    OpenGL window hints

--- src/internal/Window.cpp
+++ src/internal/Window.cpp
@@ -6,6 +6,11 @@ namespace gengine {
 		glfwInit();
 
 		glfwDefaultWindowHints();
+
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 		window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
 		glfwShowWindow(window);
 		glfwMakeContextCurrent(window);
