08ad55e7eb300332f6d1c8a13553baeb1772ba46
bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-11-08 18:32:37 -0500

    minor shader update

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,18 @@ using namespace gengine;
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"out vec3 fragPos;\n"
 "void main()\n"
 "{\n"
+"   fragPos = aPos;\n"
 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 "}\0";
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 fragPos;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = vec4(fragPos, 1.0f);\n"
 "}\n\0";
 
 bool hasArgument(int argc, char* argv[], const std::string& longForm, const std::string& shortForm) {
