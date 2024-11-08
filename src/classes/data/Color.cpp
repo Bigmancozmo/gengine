9e348aef6f7551b125d0e7fdf76ee1bfd7c256ca
Bigmancozmo <67850768+Bigmancozmo@users.noreply.github.com>
2024-11-07 21:07:26 -0500

    untested functions!

--- src/classes/data/Color.cpp
+++ src/classes/data/Color.cpp
@@ -5,6 +5,9 @@ namespace gengine {
   return Color(r_/255, g_/255, b_/255);
  }
  ColorRGB Color::toRGB() 
-  return ColorRGB
+  return ColorRGB(
+   std::round(r*255),
+   std::round(g*255),
+   std::round(b*255))
  }
 }
