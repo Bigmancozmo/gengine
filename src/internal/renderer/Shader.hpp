a7127243b11a01c5435a4c3174e2f12c27df7606
Bigmancozmo <bigmancozmo@proton.me>
2025-05-20 16:48:31 -0400

    add more functions

--- src/internal/renderer/Shader.hpp
+++ src/internal/renderer/Shader.hpp
@@ -2,6 +2,7 @@
 #define SHADER_HPP
 
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
 #include <string>
 #include <fstream>
@@ -15,11 +16,25 @@ namespace gengine {
 			Logger* logger;
 		public:
 			unsigned int ID;
+
 			Shader(const char* vertexPath, const char* fragmentPath);
+
 			void use();
+
 			void setBool(std::string& name, bool value);
 			void setInt(std::string& name, int value);
 			void setFloat(std::string& name, float value);
+
+			void setVec2(std::string& name, glm::vec2& value);
+			void setVec2(std::string& name, float x, float y);
+			void setVec3(std::string& name, glm::vec3& value);
+			void setVec3(std::string& name, float x, float y, float z);
+			void setVec4(std::string& name, glm::vec4& value);
+			void setVec4(std::string& name, float x, float y, float z, float w);
+
+			void setMat2(std::string& name, glm::mat2& mat);
+			void setMat3(std::string& name, glm::mat3& mat);
+			void setMat4(std::string& name, glm::mat4& mat);
 	};
 }
 
